df25c576a4ffc51de8f728858669955a6c1502c7
Asit Kumar Parida <asparida@microsoft.com>
2016-02-12 15:10:56 +0530

    Change

--- Source.PieMix/Source.PieMix/dist/js/app.js
+++ Source.PieMix/Source.PieMix/dist/js/app.js
@@ -3,18 +3,18 @@
     var self = this;
     self.pieData = [
         {
-            'id': '101', 'title': 'ng-attr-x', 'value': 60, 'color': '#34495e', 'child': [
+            'id': '101', 'title': '# 34495e', 'value': 60, 'color': '#34495e', 'child': [
                 {
-                    'id': '201', 'title': 'ng-attr-x', 'value': 40, 'color': '#3498db', 'child': [
-                    { 'id': '301', 'title': 'ng-attr-x', 'value': 67, 'color': '#f39c12' },
-                    { 'id': '302', 'title': 'ng-attr-x', 'value': 33, 'color': '#e74c3c' }
+                    'id': '201', 'title': '# 3498db', 'value': 40, 'color': '#3498db', 'child': [
+                    { 'id': '301', 'title': '# f39c12', 'value': 67, 'color': '#f39c12' },
+                    { 'id': '302', 'title': '# e74c3c', 'value': 33, 'color': '#e74c3c' }
                     ]
                 },
-                { 'id': '202', 'title': 'ng-attr-x', 'value': 60, 'color': '#9b59b6' }
+                { 'id': '202', 'title': '# 9b59b6', 'value': 60, 'color': '#9b59b6' }
             ]
         },
-        { 'id': '102', 'title': 'ng-attr-x', 'value': 20, 'color': '#2ecc71' },
-        { 'id': '103', 'title': 'ng-attr-x', 'value': 20, 'color': '#16a085' }
+        { 'id': '102', 'title': '# 2ecc71', 'value': 20, 'color': '#2ecc71' },
+        { 'id': '103', 'title': '# 16a085', 'value': 20, 'color': '#16a085' }
     ];
 
     self.click = function (id) {
